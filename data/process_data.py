a1fe855846ba9e1508726c94d5732ab4b21f4601
VUPT16 <VUPT16@fsoft.com.vn>
2022-11-12 23:12:43 +0700

    Update process_data.py v1

--- data/process_data.py
+++ data/process_data.py
@@ -1,12 +1,36 @@
 import sys
+import pandas as pd
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # Get msg from csv file
+    msg = pd.read_csv(messages_filepath)
+    
+    # get categories from csv file
+    cg = pd.read_csv(categories_filepath)
+    
+    # merge categories and msg by id
+    output = pd.merge(msg, cg, on="id")
+    return output
 
 
 def clean_data(df):
-    pass
+    #  get detail data
+    cg = df.categories.str.split(";",expand=True)
+    r = cg.loc[0,:]
+    cg.columns = r.apply(lambda x: x[:-2]).values
+    
+    # Change data value
+    for col in cg:
+        cg[col] = cg[col].str[-1]
+        cg[col] = cg[col].astype(int)
+    cg["related"] = cg["related"].apply(lambda x: 0 if x ==0 else 1)
+    
+    # clean data
+    df = df.drop(columns = ["categories"])
+    df = pd.concat([df, cg], axis= 1,sort=False)
+    df = df.drop_duplicates(subset=['message'], keep='last')
+    return df
 
 
 def save_data(df, database_filename):
