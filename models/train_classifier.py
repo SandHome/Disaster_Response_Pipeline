ce7accf3bd81fcd31360a25897f4cae609e61a48
VUPT16 <VUPT16@fsoft.com.vn>
2022-11-13 00:02:16 +0700

    Update ML and gitignore

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,60 @@
 import sys
+import pandas as pd
+import sqlalchemy
 
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+import pickle
 
 def load_data(database_filepath):
-    pass
+    # Get data from db file
+    database = sqlalchemy.create_engine('sqlite:///'+ str(database_filepath))
+    df = pd.read_sql_table('disastermsg', database)
+    X = df.message
+    Y = df.drop(columns = ["id","message", "genre", "original"])
+    category_names = Y.columns.values
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    # Write a tokenization function to process your text data
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    # Build a machine learning pipeline
+    output = Pipeline([('vect', CountVectorizer(tokenizer=tokenize)),('tfidf', TfidfTransformer()),('clf', MultiOutputClassifier(RandomForestClassifier()))])
+    return output
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pre = model.predict(X_test)
+    for ind,col in enumerate(category_names):
+        print(col, classification_report(Y_test.values[:,ind],Y_pre[:,ind]))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
